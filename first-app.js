ddbd5c7727b43c440029f8a0172386e2327fae4d
Ankit2708 <shivhare.ankit476@gmail.com>
2022-04-07 22:14:51 +0530

    nodecomit

---
+++ first-app.js
@@ -0,0 +1,4 @@
+console.log('Hello world');
+ console.log('Hello nodejs');
+const fs=require('fs');//keyword tocreate file system
+fs.writeFileSync('Hello txt','Hello from Nodejs');//will create a text file
